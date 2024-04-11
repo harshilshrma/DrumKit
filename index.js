12c1338b4c80475a3618ba76708c99ab05a70a44
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-04-11 20:37:42 +0530

    project completed successfully!

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ for (var i = 0; i < numOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 
 }
 
 document.addEventListener("keydown", function (event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 
@@ -53,4 +55,12 @@ function makeSound(key) {
 
         default:
     }
+}
+
+function buttonAnimation (currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add('pressed');
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 200);
 }
